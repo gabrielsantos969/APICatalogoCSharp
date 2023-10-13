6af6ba218e65ea56ba0867f1a361ade99b734462
Gabriel Santos <gabrielsantos98898@outlook.com>
2023-10-13 18:11:56 -0300

    Adicionada nova rota para post de produtos, criacao e adicionado o JsonIgnore em Categoria do model Produtos

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -26,5 +26,31 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
+        [HttpGet("{id:int}", Name= "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p=> p.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+
+            if(produto is null)
+            {
+                return BadRequest();
+            }
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId },produto);
+        }
+
     }
 }
--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models
 {
@@ -31,6 +32,7 @@ namespace APICatalogo.Models
 
         public int CategoriaId { get; set; }
 
+        [JsonIgnore]
         public Categoria? Categoria { get; set; }
     }
 }
