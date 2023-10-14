0d51b1fc9096e6a885dd9d24f5d06287136aaef9
Gabriel Santos <gabrielsantos98898@outlook.com>
2023-10-14 11:03:15 -0300

    Criado o controller de categoria e o metodo de puxar todas as categorias

---
+++ APICatalogo/Controllers/CategoriaController.cs
@@ -0,0 +1,33 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _context.Categorias.ToList();
+
+            if(categorias is null)
+            {
+                return NotFound("Nenhuma categoria encontrada...");
+            }
+
+            return Ok(categorias);
+        }
+
+    }
+}
